b23fd75881d179c9e205ebbdff7a275257e7eaee
marcosvmg <devmarcosvmg@gmail.com>
2025-05-27 14:16:54 -0300

    corrigido2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 
@@ -40,43 +39,3 @@
 
 
 @endsection
-=======
-
-    <style>
-.container{
-    display:flex;
-}
-        .card{
-         padding:1rem;
-         margin:0.6rem;
-         display:flex;
-         justify-content:center;
-        }
-    </style>
- 
-    
-
-
- @extends('template')
-@section('titulo', 'Sobre')
-@section('conteudo')
-<div class="container">
-@foreach ($cards as $h)
-
-<div class="card">
-<img class="card-img-top" src="{{$h['imagem']}}" alt="Card image" style="width:10rem">
-    <div class="card-body">
-        <h4 class="card-title"><p>{{$h['nome']}}</p></h4>
-        <p class="card-text">{{$h['texto']}}</p>
-       <h5 class="card-title"><p>{{$h['preco']}}</p></h5>
-       <button class="btn btn-danger"> Saiba mais</button>
-    </div>
-  </div>
-@endforeach
-    </div>
-
-
-@endsection
-
-
->>>>>>> 814161f31cb024661fb29cf301d5536652a0276c
